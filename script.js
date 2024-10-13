39d5539979a59bfc34e84b412ae2572dc4831bc2
Eduardo Lucio de Oliviera <eduardo.oliveiira2005@gmail.com>
2024-10-13 18:57:12 -0300

    Ending coding Page
    The Last commit for "mobile" configurate page

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const headerButton = document.querySelector("#buttonHeader");
 const containerHeader = document.querySelector(".header_home");
-const iconIndex = document.querySelector(".header_home--link");
 
 const taskTitle = document.getElementById("inpKey");
 const taskInput = document.getElementById("inpValue");
@@ -20,13 +19,13 @@ headerButton.addEventListener('click', () => {
     if (formTask.style.width === "100vw") {
         // Ocultar o sidebar/ Form "LOCAL STORAGE"
         formTask.style.width = "0";
-        headerButton.textContent = "Create new Task'/";
     } else {
         // Mostrar o sidebar/ Form "LOCAL STORAGE"
         formTask.style.width = "100vw";
-        headerButton.textContent = "My Tasks/";
     }
-});
+}, 20000);
+
+
 
 
 function verificacaoPrompt() {
@@ -83,19 +82,61 @@ function editTask(index) {
     }
 }
 
-function exibeDataInformation() {
+function exibeDataInformation() { 
     taskList.innerHTML = "";
 
     tasks.forEach((task, index) => {
         const div = document.createElement("div");
         div.classList.add("container_Task");
+        div.setAttribute('data-index', index);  
         div.innerHTML = `
             <span>${task.title}</span>
             <hr>
             <button class="edit-button" onclick="editTask(${index})">Edit</button>
             <button class="delet-button" onclick="removendoTask(${index})">Delete</button>`;
-
         taskList.appendChild(div);
-    });
-}
+
+        div.addEventListener('click', function(event) {
+            const el = event.target;  // Elemento que foi clicado
+            
+            if (el.tagName === 'DIV') {  // Verifica se o elemento clicado é uma <DIV>
+                const index = el.getAttribute('data-index');  // Pega o índice do data-index
+                
+                // Busca o item do localStorage pelo índice 
+                const selectedItem = tasks[index];  
+
+                if (selectedItem) {  // Se existir um item com esse índice
+                    const listingReceita = document.getElementById('taskListAll');
+                    listingReceita.innerHTML = ""; 
+                    
+                    const div = document.createElement("div");
+                    div.innerHTML = `
+                        <header>
+                            <a href="home.html">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" id="img">
+                                    <path fill="#000000" d="M125.7 160l50.3 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L48 224c-17.7 0-32-14.3-32-32L16 64c0-17.7 14.3-32 32-32s32 14.3 32 32l0 51.2L97.6 97.6c87.5-87.5 229.3-87.5 316.8 0s87.5 229.3 0 316.8s-229.3 87.5-316.8 0c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0c62.5 62.5 163.8 62.5 226.3 0s62.5-163.8 0-226.3s-163.8-62.5-226.3 0L125.7 160z"/>
+                                </svg>
+                                <a href="home.html" class="home--link"><span>// My Tasks///</span></a>
+                            </a>
+                        </header>
+                        <section class="section_receitas--open">
+                            <div class="section_receitas--title">
+                                <span>${selectedItem.title}</span>
+                            </div>
+                            <div class="section_receitas--edit">
+                                <button class="edit_button--open" onclick="editTask(${index})">Edit</button>
+                            </div>
+                        </section>
+                        <br>
+                        <br>
+                        <span class="task_text--open">${selectedItem.text}</span>`;
+                    listingReceita.appendChild(div);
+                    listingReceita.classList.add('active');
+                }
+            }
+        }); 
+    });   
+};
+
+
 exibeDataInformation();
